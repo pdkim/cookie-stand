ac2a4a556d84960e0f4dc9190bb9f46098204140
Phillip Kim <phildkim@gmail.com>
2018-01-23 20:18:32 -0800

    got an object and their methods 'working' in console

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 'use strict';
 
-var pike = {
-  var minCustomers = 23,
-  var maxCustomers  = 65,
-  var avgSales = 6.3,
-  var storeHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM'],
-  var cookiePerHr = [16, 20, 35, 48, 56, 77, 93, 144, 119, 84, 61, 23, 42, 57, 29],
-  hourlyCookies: function() {
-    for(var i = 0; i <= storeHours.length; i++) {
-      console.log('At ' + storeHours[i] + ', you have ' + cookiePerHr[i] + ' cookies.');
+var patStores = {
+  storeHours: ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM'],
+  pike: {
+    minCustomers: 23,
+    maxCustomers: 65,
+    cookiePerHr: [16, 20, 35, 48, 56, 77, 93, 144, 119, 84, 61, 23, 42, 57, 29],
+    hourlyCookies: function() {
+      for(var i = 0; i < patStores.storeHours.length; i++) {
+        console.log('Pat\'s store on Pike sold ' + patStores.pike.cookiePerHr[i] + ' cookies at ' + patStores.storeHours[i] + '.');
+      }
+    },
+    calcAvg: function() {
+      function addCookie(a, b) {
+        var addition = a + b;
+        return addition;
+      }
+      var totalCookies = 0;
+      for(var i = 0; i < patStores.pike.cookiePerHr.length; i++) {
+        totalCookies = addCookie(patStores.pike.cookiePerHr[i], totalCookies);
+      }
+      var avgCookies = totalCookies / patStores.pike.cookiePerHr.length;
+      console.log(Math.round(avgCookies));
     }
   }
-}
+};
 
-pike.hourlyCookies();
+patStores.pike.hourlyCookies();
+patStores.pike.calcAvg();
