cf091a16cfac90f6d8c9b742acb4ede2f37fc748
Phillip Kim <phildkim@gmail.com>
2018-02-01 19:08:29 -0800

    event is not breaking the table code, but it is not populating in the table.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var storeHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2P
 
 var allStores = [];
 
+var salesChart = document.getElementById('salesChart');
+
 //constructor
 function MakeLocation(
   name,
@@ -22,7 +24,7 @@ function MakeLocation(
   this.sumAllCustomers = function() {
     for(var a = 0; a < storeHours.length; a++) {
       this.customerPerHr.push(Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers));
-      console.log(this.customerPerHr[a]);
+      //console.log(this.customerPerHr[a]);
     }
   };
   this.randCookies = function() {
@@ -30,7 +32,7 @@ function MakeLocation(
       var countCookies = (Math.round(this.avgCookies * this.customerPerHr[b]));
       this.cookiesSoldPerHr.push(countCookies);
       this.cookieTotal += countCookies;
-      console.log(this.cookiesSoldPerHr[b]);
+      //console.log(this.cookiesSoldPerHr[b]);
     }
   };
   this.sumAllCustomers();
@@ -113,57 +115,37 @@ function makeFooterRows() {
   salesChart.appendChild(trElement);
 }
 
-//Event starts here
-// var addRow = document.getElementById('salesChart');
-
-//function for adding new row
-// function addNewRow() {
-//   var salesChart = document.getElementById('salesChart');
-//   var trElement = document.createElement('tr');
-//   var storeName = allStores[allStores.length];
-//   var tdElement = document.createElement('td');
-//   tdElement.textContent = storeName.name;
-//   trElement.appendChild(tdElement);
-
-//   for(var e = 0; e < storeHours.length; e++) {
-//     tdElement = document.createElement('td');
-//     var cookPerHour  = storeName.cookiesSoldPerHr[e];
-//     tdElement.textContent = cookPerHour;
-//     trElement.appendChild(tdElement);
-//   }
-//   tdElement = document.createElement('td');
-//   tdElement.textContent = storeName.cookieTotal;
-//   trElement.appendChild(tdElement);
-//   salesChart.appendChild(trElement);
-// }
-
-//event
-// function handleSubmit(event) {
-//   console.log(event);
-//   event.preventDefault();
-
-//   if(!event.target.name.value) {
-//     return alert('Must enter a store location.');
-//   }
-//   if(!event.target.minCustomers.value || !event.target.maxCustomers.value || !event.target.avgCookies.value){
-//     return alert('Must enter a numerical value.');
-//   }
-//   var newLocation = new MakeLocation(this.name, this.minCustomers, this.maxCustomers, this.avgCookies);
-//   event.target.name.value = null;
-//   event.target.minCustomers.value = null;
-//   event.target.maxCustomers.value = null;
-//   event.target.avgCookies.value = null;
-
-//   allStores.push(newLocation);
-//   console.log(newLocation);
-//   addNewRow();
-// }
-
-// //event listener
-// addRow.addEventListener('submit', handleSubmit());
-
-
 makeStore();
 makeHeaderRows();
 makeBodyRows();
 makeFooterRows();
+
+//Event starts here
+function handleSubmit(event) {
+  event.preventDefault();
+
+  var name = event.target.allStores.name;
+  var minCustomers = parseInt(event.target.allStores.minCustomers);
+  var maxCustomers = parseInt(event.target.allStores.maxCustomers);
+  var avgCookies = parseFloat(event.target.allStores.avgCookies);
+
+  if(!event.target.name.value || !event.target.minCustomers.value || !event.target.maxCustomers.value || !event.target.avgCookies.value){
+    return alert('Must enter a numerical value.');
+  }
+
+  var newLocation = new MakeLocation(name, minCustomers, maxCustomers, avgCookies);
+
+  event.target.name.value = null;
+  event.target.minCustomers.value = null;
+  event.target.maxCustomers.value = null;
+  event.target.avgCookies.value = null;
+
+  allStores.push(newLocation);
+  console.log(newLocation);
+  makeBodyRows();
+}
+
+//event listener
+salesChart.addEventListener('submit', handleSubmit);
+console.log(salesChart);
+
